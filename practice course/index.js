a6080ccb378e707012bd4e74396fd72533f52f23
yumnamunirahmed <yumnamunirahmed@gmail.com>
2024-10-10 19:19:48 +0500

    Add files via upload

---
+++ practice course/index.js
@@ -0,0 +1,131 @@
+//   //    chapter no 01
+//            // QUESTION NO :01
+// //   alert is a function that popap as message 
+//             // QUESTION NO :02
+//             alert ("welcome to my website");
+//             // QUESTION NO :03
+//             // If we capitalize the keyword alert the website shown error or its cant work beacuse javascript is case sensitive
+//             // Alert ("welcome to my website");
+//             // QUESTION NO :04
+//             alert ("hello,world!");
+//             // Question no :05
+//             alert ("Hello World!");
+//             // alert = popap message 
+//             // () = opening and closing parathesis
+//             // "Hello World!"= message in string 
+//         //  chapter no 02
+//         // QUESTION NO :01
+//         var message= "hello,World";
+//         alert (message)
+//         // QUESTION NO :02
+//         var age;
+//         age = 25
+//             alert (age);
+        
+//         // Q UESTION NO 03
+//         var greeting ="Good morning"
+//         alert (greeting)
+//         greeting = "good evening"
+//         alert(greeting)
+//        // QUESTION NO 04
+//         var favouriteColor = "blue";
+//         alert( `My favourite color is ${favouriteColor}`)
+// //    CHAPTER NO : 03 
+// // QUESTION NO: 01
+// var height = 170
+// var newHeight = height+10;
+// alert (newHeight);
+// // QUESTION NO: 02
+// var num1 =50;
+// var num2= 30;
+// var sum= num1 + num2 ;
+// alert (sum);
+// // QUESTION NO: 03
+// var numToBeAdded=20;
+// var total = 100;
+// alert (total);
+// // QUESTION NO: 04
+// var price = 120;
+// var tax =price * 0.10;
+// var totalPrice= price + tax;
+// alert (totalPrice);
+// //    CHAPTER NO : 04
+// // QUESTION NO: 01
+//  var myAddress="Pakistan"
+//  alert (myAddress);
+// // QUESTION NO: 02
+//   var userName = "yumnaMunirAhmed"
+//   var userEmail = "yumnaMunirAhmed@gamil.com"
+//   var userPassword = "12345"
+//   alert ("userName:" + userName + "/nEmail:" + userEmail + "/nPassword:" + userPassword);
+// // QUESTION NO: 03
+// //  Incorrect
+// var Useraddress = "123 main street "
+// // Correct  
+// var userAddress = "123 main street "
+// // QUESTION NO: 04
+//  var rose = "floribundas"
+//  alert (rose)
+//  alert(rose.toUpperCase)
+// //   CHAPTER NO : 05
+//  // QUESTION NO: 01
+// var sum = 25 + 30;
+// alert (sum)
+// // QUESTION NO: 02
+// var num1 = 25
+// var num2 = 5
+// var sum = num1 + num2 
+// alert (sum) 
+// // QUESTION NO: 03
+// var modulusResult  = 27 % 4 
+// alert (modulusResult)
+// // CHAPTER NO : 06
+// // QUESTION NO: 01
+// var num = 3 
+// var newNum = num++
+// alert (num)
+// alert (newNum)
+// // QUESTION NO: 02
+// var cunter = 10
+// cunter++
+// cunter++
+// alert (cunter)
+// // QUESTION NO: 03
+// var point = 20
+// point--
+// ++point
+// alert (point)
+// // QUESTION NO: 04
+// var number = 0
+// ++number
+// ++number
+// ++number
+// alert (number)
+// CHAPTER NO : 07
+// QUESTION NO : 01
+var result = (20 % 6) + 10 * 2
+// QUESTION NO : 02
+var expressionValue = (6 + 2) * (5 - 3)
+// QUESTION NO : 03
+alert (result)
+// QUESTION NO : 04
+alert (expressionValue)
+// CHAPTER NO : 08
+// QUESTION NO : 01
+var usersName =prompt("what is your name?")
+alert("hello,"+ usersName +"!")
+// QUESTION NO : 02
+var ages =prompt ("How old are you?")
+if(ages===null){
+alert("No age provided")
+}else{
+  ("you are"+ages+"years old")
+}
+// QUESTION NO : 03
+var pet = prompt("How many pets do you have?")
+if(pet===""){
+  alert ("you didnot enter anything")
+}else{
+  ("you have"+pet+"pet")
+}
+
